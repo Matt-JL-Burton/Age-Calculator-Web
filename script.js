174695f97f7e5f83679e96a9262bd78c6b1b1846
Matt-JL-Burton <mat@emux.app>
2021-12-10 21:41:46 +0000

    made teh code kinda work

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var listOfPrime = [2];
-// while (true) {
-//     var initialConstOfMultiplication = 1;
-//     for (var i = 0; i < listOfPrime.length; i++){
-//         initialConstOfMultiplication = initialConstOfMultiplication * listOfPrime[i]
-//     } 
-//     listOfPrime.push(initialConstOfMultiplication)
-//     console.log(listOfPrime)
-// }
+while (true) {
+    var initialConstOfMultiplication = 1;
+    for (var i = 0; i < listOfPrime.length; i++){
+        initialConstOfMultiplication = initialConstOfMultiplication * listOfPrime[i]
+    } 
+    listOfPrime.push(initialConstOfMultiplication+1)
+    console.log(listOfPrime)
+}
