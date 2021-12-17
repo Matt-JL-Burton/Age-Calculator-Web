852e0ab2b45e9ff5ac3b92591d2b5261b1eed865
Matt-JL-Burton <mat@emux.app>
2021-12-17 09:28:16 +0000

    caned the script file from calculating primes to nothing

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function workOutPrime() {
-    var multiplactionOfPreviousPrimes = 1
-    for (var i = 0; i < listOfPrimes.length; i ++){
-        multiplactionOfPreviousPrimes = multiplactionOfPreviousPrimes *  listOfPrimes[i]
-    }
-    listOfPrimes.push(multiplactionOfPreviousPrimes + 1)
-    console.log(listOfPrimes)
-};
 
-let listOfPrimes = [2];
-
-setInterval(workOutPrime,5000);
-// streaking 2
