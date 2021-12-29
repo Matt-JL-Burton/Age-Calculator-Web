30195b5b2a6bfd2ddfe98810343325802337c6a7
Matt-JL-Burton <mat@emux.app>
2021-12-29 10:55:47 +0000

    added functionality for going to the next page

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function getDataFromDateBox() {
     var entereDate = document.getElementById('MainDate').value;
     // console.log(entereDate)
-    // if data is Valid then go to show metrics Page
+    // if data is Valid then go to show metrics Page (code below)
+    currentURLList = window.location.href.split('/')    
+    currentURLList[currentURLList.length-1] = 'metrics.html'
+    newURL = currentURLList.join('/')
+    window.location.href = newURL
 };
