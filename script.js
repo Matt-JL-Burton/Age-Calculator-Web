5c9eea86b3a5049f621cb6ef0e0eb40c0950b498
Matt-JL-Burton <mat@emux.app>
2021-12-29 14:28:04 +0000

    explored

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function getDataFromDateBox() {
-    var entereDate = document.getElementById('MainDate').value;
-    // console.log(entereDate)
-    // if data is Valid then go to show metrics Page (code below)
-    currentURLList = window.location.href.split('/')    
-    currentURLList[currentURLList.length-1] = 'metrics.html'
-    newURL = currentURLList.join('/')
-    window.location.href = newURL
+    var enteredDate = document.getElementById('MainDate').value;
+    console.log(enteredDate)
+    console.log(console.log(moment(enteredDate,"DD/MM/YYYY", true).isValid))
+
+    // currentURLList = window.location.href.split('/')    
+    // currentURLList[currentURLList.length-1] = 'metrics.html'
+    // newURL = currentURLList.join('/')
+    // window.location.href = newURL
 };
+
+// TODO: 
+// need to learn to import modules
