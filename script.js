d767c02fb8a89d9d571a94bb9610da024545f9a2
Matt-JL-Burton <mat@emux.app>
2021-12-30 13:11:47 +0000

    fixed checking for valid date

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function getDataFromDateBox() {
-    var enteredDate = document.getElementById('MainDate').value;
-    console.log(enteredDate)
-    console.log(console.log(moment(enteredDate,"DD/MM/YYYY", true).isValid))
-
-    // currentURLList = window.location.href.split('/')    
-    // currentURLList[currentURLList.length-1] = 'metrics.html'
-    // newURL = currentURLList.join('/')
-    // window.location.href = newURL
+    var enteredDate = (document.getElementById('MainDate').value).replace(/-/g,"/");
+    if (!(isNaN(Date.parse(enteredDate)))) {
+        console.log('Valid date')
+        currentURLList = window.location.href.split('/')    
+        currentURLList[currentURLList.length-1] = 'metrics.html'
+        newURL = currentURLList.join('/')
+        window.location.href = newURL
+    } else {
+        console.log("invalid data")
+        // add error message
+    }
 };
 
 // TODO: 
