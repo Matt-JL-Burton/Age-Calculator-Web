b8c9540b40c420fc3982cb453d385ea8069a7589
Matt-JL-Burton <mat@emux.app>
2022-01-05 08:53:52 +0000

    lots of cool work

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function getDataFromDateBox() {
     var enteredDate = (document.getElementById('mainDate').value).replace(/-/g,"/");
     if (!(isNaN(Date.parse(enteredDate)))) {
-        console.log('Valid date')
+        document.getElementById('MainPageMainDiv').style.display = "none"
+        document.getElementById('mainPageLoadingDiv').style.display = "flex"
         currentURLList = window.location.href.split('/')    
         currentURLList[currentURLList.length-1] = 'metrics.html'
         newURL = currentURLList.join('/')
