082d8889697b44638c10dcb4cc036aecade52a6b
Matt-JL-Burton <mat@emux.app>
2022-01-11 10:27:52 +0000

    work no succesful

--- metrics.js
+++ metrics.js
@@ -1,2 +1,6 @@
 let date = localStorage.getItem('Date')
 document.getElementById('dateDisplay').innerHTML = date
+var formattedDate = (document.getElementById('mainDate').value).replace(/-/g,"-"); //FIXME: need to replace all / with - in a string 
+console.log(formattedDate)
+// console.log(date.split('/')[0],date.split('/')[1],date.split('/')[2])
+// console.log(timeSinceEpoch)
