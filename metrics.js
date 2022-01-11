8a9dda50e8f5b341ca84323d82d3c54b262b7a43
Matt-JL-Burton <mat@emux.app>
2022-01-11 10:39:23 +0000

    great progress

--- metrics.js
+++ metrics.js
@@ -1,6 +1,5 @@
 let date = localStorage.getItem('Date')
 document.getElementById('dateDisplay').innerHTML = date
-var formattedDate = (document.getElementById('mainDate').value).replace(/-/g,"-"); //FIXME: need to replace all / with - in a string 
-console.log(formattedDate)
-// console.log(date.split('/')[0],date.split('/')[1],date.split('/')[2])
-// console.log(timeSinceEpoch)
+var formattedDate = new Date(date.replace(/\//g,"-")); //FIXME: need to replace all / with - in a string 
+const timeSinceEpochinMS = formattedDate.getTime()
+console.log(timeSinceEpochinMS)
