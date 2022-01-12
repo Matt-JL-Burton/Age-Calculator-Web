9ecccf86b5415898e5abd3713e4867a91a57c31c
Matt-JL-Burton <mat@emux.app>
2022-01-12 10:52:55 +0000

    Added time since now in ms

--- metrics.js
+++ metrics.js
@@ -1,5 +1,14 @@
 let date = localStorage.getItem('Date')
-document.getElementById('dateDisplay').innerHTML = date
+document.getElementById('dateDisplay').innerHTML = "Entered Date : " + String(date)
+
 var formattedDate = new Date(date.replace(/\//g,"-"));
-const timeSinceEpochinMS = formattedDate.getTime()
-console.log(timeSinceEpochinMS)
+const timeSinceEpochinMSForEntered = formattedDate.getTime()
+var formattedDateNow = new Date()
+const timeSinceEpochinMSForNow = new Date()
+var timeDifference = timeSinceEpochinMSForEntered - timeSinceEpochinMSForNow
+document.getElementById('timeSinceNowms').innerHTML = "Time since now : " + String(timeDifference)
+
+// FIXME: need to fix this to find the difference in time between two dates
+var year = Math.floor(timeDifference / 31556952000.0) 
+var remainingTime = timeDifference - (year * 31556952000.0)
+var months = Math.floor(timeDifference)
