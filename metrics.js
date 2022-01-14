2497752ccef8b1ad667cf2c276130d021326ff7b
Matt-JL-Burton <mat@emux.app>
2022-01-14 09:29:42 +0000

    did work

--- metrics.js
+++ metrics.js
@@ -1,18 +1,19 @@
 function workOutCompleteMonthsPass(timeInMs,startDate,completedyears) {
+    var listOfDate = startDate.split("/")
     let listOfMothsDates = [31,28,31,30,31,30,31,31,30,31,30,31]
 }
 
 let date = localStorage.getItem('Date')
-document.getElementById('dateDisplay').innerHTML = "Entered Date : " + String(date)
+document.getElementById('dateDisplay').innerHTML = "Entered Date : " + String(String(date).split("/").reverse()).replace(/,/g,"/")
 
 var formattedDate = new Date(date.replace(/\//g,"-"));
 const timeSinceEpochinMSForEntered = formattedDate.getTime()
 var formattedDateNow = new Date()
 const timeSinceEpochinMSForNow = new Date()
 var timeDifference = timeSinceEpochinMSForEntered - timeSinceEpochinMSForNow
+console.log(timeDifference)
 document.getElementById('timeSinceNowms').innerHTML = "Time since now : " + String(timeDifference) + "ms"
 
-// FIXME: need to fix this to find the difference in time between two dates
 var completeYears = Math.floor(timeDifference / 31556952000.0)
 var remainingTime = timeDifference - (completeYears * 31556952000.0)
 
